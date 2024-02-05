efea174d1dd4d04e6e0c7d4feb1647ece4c91f27
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-06 01:08:55 +0530

    file upload functionality using multer

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+const upload = multer({ storage });
+
+// const upload = multer({ dest: '/uploads' });
 
 const PORT = 8080;
-
 
 const app = express();
 
 app.use(cors());
 
-// All the below API's are fake 
-
 app.post("/upload", (req, res) => {
-    
-    setTimeout(() => {
-        console.log('file uploaded')
-        return res.status(200).json({ result: true, msg: 'file uploaded' });
-    }, 3000);
+  setTimeout(() => {
+    console.log("file uploaded");
+    return res.status(200).json({ result: true, msg: "file uploaded" });
+  }, 3000);
 });
 
-app.delete("/upload", (req, res) => {
-    console.log(`File deleted`)
-    return res.status(200).json({ result: true, msg: 'file deleted' });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.json(req.file);
 });
 
+app.delete("/delete", (req, res) => {
+  console.log(`File deleted`);
+  return res.status(200).json({ result: true, msg: "file deleted" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-})
+  console.log(`Server is running on port: ${PORT}`);
+});
--- frontend/file-upload-react/src/Components/FileUpload/FileUpload.jsx
+++ frontend/file-upload-react/src/Components/FileUpload/FileUpload.jsx
@@ -4,13 +4,35 @@ import { PlusCircle } from "lucide-react";
 import './FileUpload.scss';
 
 const FileUpload = ({ files, setFiles }) => {
-  const uploadHandler = () => {};
+  const uploadHandler = (event) => {
+    const file = event.target.files[0];
+    file.isUploading = true;
+    setFiles(...files, file);
+
+    //Upload file
+    const formData = new FormData();
+    formData.append(
+        file.name,
+        file,
+        file.name
+    );
+
+    axios.post("http://localhost:8080/upload", formData)
+    .then((res) => {
+        file.isUploading = false;
+    setFiles(...files, file);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+
+  };
 
   return (
     <>
       <div className="file-card">
         <div className="file-inputs">
-          <input type="file" />
+          <input type="file" onChange={uploadHandler} />
           <button>
             <i>
               <PlusCircle />
